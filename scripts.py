660e4b7b86a16dc033e8af0c1dc978657f08e492
Enrique <fenriquegimenez@gmail.com>
2020-07-06 11:35:26 -0400

    Credit calculator finished! Woohoo! 🥳🥳

--- scripts.py
+++ scripts.py
@@ -1,7 +1,23 @@
-print('Ingrese el monto que desea solicitar:')
-monto_prestamo = int(input())
-print('Qué quiere calcular?')
-print('Escriba "m" - para conteo de meses \nEscriba "p" - para cuota mensual:')
-tipo_de_calculo = input()
-print('Ingrese la cantidad de meses deseada:')
-duracion_deseada = int(input())
+import math
+
+print('Enter the credit principal:')
+credit_principal = int(input())
+print('What do you want to calculate?')
+print('Type "m" for count of months \nType "p" - for monthly payment:')
+calc_type = input()
+
+if calc_type == 'm':
+    print('Enter the monthly payment:')
+    user_monthly_payment = int(input())
+    credit_repay = credit_principal / user_monthly_payment
+    if credit_repay == 1:
+        print(f'It takes 1 month to repay the credit')
+    elif credit_repay > 1:
+        print(f'It takes {math.ceil(credit_repay)} months to repay the credit')
+elif calc_type == 'p':
+    print('Enter the count of months:')
+    user_duration = int(input())
+    monthly_payment = math.ceil(credit_principal / user_duration)
+    last_payment = credit_principal - (user_duration - 1) * monthly_payment
+    print(
+        f'Your monthly payment = {monthly_payment} with last payment = {last_payment}')
