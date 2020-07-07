23fb6039805a93146feac3888a7b093b2664c29b
Enrique <fenriquegimenez@gmail.com>
2020-07-07 12:01:01 -0400

    second part finished!

--- credit-calculator.py
+++ credit-calculator.py
@@ -16,7 +16,9 @@ if calc_choice == 'n':
     print('Enter credit interest:')
     credit_interest = int(input())
     nominal_interest_rate = (credit_interest / (12 * 100))
-    total_months = round(math.log(monthly_payment / (monthly_payment - nominal_interest_rate * credit_principal), 1 + nominal_interest_rate))
+    total_months = round(math.log(monthly_payment / (monthly_payment 
+    - nominal_interest_rate * credit_principal), 
+    1 + nominal_interest_rate))
     years = total_months // 12
     months = total_months % 12
     if years <= 1:
@@ -33,11 +35,14 @@ if calc_choice == 'n':
         print(f'You need {years} years and {months} months to repay this credit!')
 elif calc_choice == 'a':
     print('Enter the credit principal:')
-    credit_principal = float(input())
+    credit_principal = int(input())
     print('Enter the count of periods:')
-    periods_count = float(input())
+    periods_count = int(input())
     print('Enter credit interest:')
-    credit_interest = float(input())
+    credit_interest = int(input())
+    nominal_interest_rate = (credit_interest / (12 * 100))
+    annuity_payment = int(math.ceil(credit_principal * ((nominal_interest_rate * (1 + nominal_interest_rate) ** periods_count) 
+    / (((1 + nominal_interest_rate) ** periods_count) - 1))))
     print(f'Your annuity payment = {annuity_payment}!')    
 elif calc_choice == 'p':
     print('Enter monthly payment:')
